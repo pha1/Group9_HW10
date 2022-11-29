d168a2e4a485357eebb99d3ec1ae890e0204411d
pha1 <89549278+pha1@users.noreply.github.com>
2022-11-28 20:08:12 -0500

    Version 0.0.1
    Updated:
    - build.gradle: google-services plugin - was causing an error for Firebase Initialization

--- app/src/main/java/com/example/group9_hw10/MainActivity.java
+++ app/src/main/java/com/example/group9_hw10/MainActivity.java
@@ -12,12 +12,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.group9_hw10.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TripsFragment.TripsFragmentListener {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.rootView, new LoginFragment(), "Login")
+                    .commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.rootView, new TripsFragment(), "Trips")
+                    .commit();
+        }
     }
 }
